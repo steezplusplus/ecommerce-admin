63846675671c203dc279a89b81748d72454720b7
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-08-13 14:15:36 -0400

    feat: Init project

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+type AuthLayoutProps = {
+  children: React.ReactNode;
+}
+
+export default function AuthLayout(props: AuthLayoutProps) {
+  const { children } = props;
+  return (
+    <main className="h-full flex items-center justify-center">
+      {children}
+    </main>
+  )
+}
---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-xl">Admin Dashboard</h1>
+      <Button>Click me</Button>
+    </main>
+  )
+}
