ddbff6ab4f26d69d2827e43cb9cd0a6bc2e01746
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-08-14 13:16:47 -0400

    feat: Install & configure Prisma

---
+++ lib/db.ts
@@ -0,0 +1,25 @@
+/**
+ * Instantiates `PrismaClient` and stores it in `globalThis`
+ * Will only instantiate PrismaClient if it's not on the globalThis object
+ * Otherwise it returns the already existing instance
+ * Basically, a singleton pattern.
+ * Docs here: https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
+ */
+
+import { PrismaClient } from "@prisma/client";
+
+const NODE_ENV = process.env.NODE_ENV;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
+
+export const prismadb = globalForPrisma.prisma ?? new PrismaClient({
+  log: NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+});
+
+if (NODE_ENV !== "production") {
+  globalForPrisma.prisma = prismadb;
+}
+
+
