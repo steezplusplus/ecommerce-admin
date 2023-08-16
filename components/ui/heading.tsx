f00ed8dadb18e3b01b4c3d5f83ab6745c42898ec
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-08-16 14:12:01 -0400

    feat: Create reusable header

---
+++ components/ui/heading.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./button";
+
+type HeadingProps = {
+  title: string;
+  description: string;
+};
+
+export function Heading(props: HeadingProps) {
+  const { title, description } = props;
+  return (
+    <header>
+      <h2 className="text-3xl font-bold tracking-tight">{title}</h2>
+      <p className="text-sm text-muted-foreground">{description}</p>
+    </header>
+  );
+}
